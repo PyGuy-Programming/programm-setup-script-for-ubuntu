a2ca73abb61cf174ca63b3a1c2eb4c5c89bbd07f
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-03 17:04:27 +0200

    commit

--- installer.sh
+++ installer.sh
@@ -3,15 +3,15 @@ set -e
 clear
 sudo apt update
 echo "installing apt packages..."
-sudo apt install -y curl blobby flatpak git gnome-shell-extension-manager gnome-shell-extensions gnome-software gnome-tweaks gufw inkscape kdeconnect libcanberra-gtk micro neofetch vlc wireplumber
+sudo apt install -y curl flatpak git gnome-shell-extension-manager gnome-shell-extensions gnome-software gnome-tweaks gufw inkscape kdeconnect libcanberra-gtk micro neofetch vlc wireplumber neovim plocate evil-winrm cmake
 echo "finished installing apt packages"
 sleep 1
 clear
 echo "downloading and installing .deb packages (steam; vs-code)..."
 wget vs-code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'   
-wget steam_latest.deb 'https://cdn.fastly.steamstatic.com/client/installer/steam.deb'
-sudo apt install ./vs-code.deb -y 
-sudo apt install ./steam_latest.deb -y
+wget 'https://cdn.fastly.steamstatic.com/client/installer/steam.deb'
+wget 'https://github.com/Zettlr/Zettlr/releases/download/v4.3.1/Zettlr-4.3.1-amd64.deb'
+sudo apt install ./*.deb -y
 echo "finished installing .deb packages"
 sleep 1
 clear
@@ -24,6 +24,11 @@ echo "installing flathub and related packages..."
 flatpak install flathub
 flatpak install flathub org.audacityteam.Audacity
 flatpak install flathub org.prismlauncher.PrismLauncher
+flatpak install flathub com.github.PintaProject.Pinta
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub fr.handbrake.ghb
+# uncomment to also install sober:
+# flatpak install flathub org.vinegarhq.Sober
 echo "finished installing flathub and related packages"
 sleep 1
 clear
@@ -50,3 +55,12 @@ else
 	echo -e "\033[0;31m exited with code 1 \033[0m"
 	exit 1
 fi
+sleep 1
+clear
+echo "installing and compiling blobby-volley2 v1.1.1..."
+tar -xvf 'blobby2-linux-1.1.1.tar.gz'
+mv 'blobby-1.1.1' '~/'
+cd '~/blobby-1.1.1/'
+cmake .
+make install
+echo "finished installing blobby-volley v1.1.1!"
