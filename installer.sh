ad52ce3f5963d3d4d3da8d331176dcd8901b7a02
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-11 18:28:48 +0200

    commit

--- installer.sh
+++ installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+touch ~/start-position
+pwd > ~/start-position
 clear
 sudo apt update
 echo "installing apt packages..."
@@ -9,16 +11,16 @@ sleep 1
 clear
 echo "downloading and installing .deb packages (steam; vs-code)..."
 wget vs-code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'   
-wget 'https://cdn.fastly.steamstatic.com/client/installer/steam.deb'
-wget 'https://github.com/Zettlr/Zettlr/releases/download/v4.3.1/Zettlr-4.3.1-amd64.deb'
+wget steam.deb 'https://cdn.fastly.steamstatic.com/client/installer/steam.deb'
+wget Zettlr-4.3.1-amd64.deb 'https://github.com/Zettlr/Zettlr/releases/download/v4.3.1/Zettlr-4.3.1-amd64.deb'
 sudo apt install ./*.deb -y
 echo "finished installing .deb packages"
-sleep 1
+sleep 2
 clear
 echo "installing snap packages (discord)..."
 sudo snap install discord
 echo "finished installing snap packages"
-sleep 1
+sleep 2
 clear 
 echo "installing flathub and related packages..."
 flatpak install flathub
@@ -30,7 +32,7 @@ flatpak install flathub fr.handbrake.ghb
 # uncomment to also install sober:
 # flatpak install flathub org.vinegarhq.Sober
 echo "finished installing flathub and related packages"
-sleep 1
+sleep 2
 clear
 echo "installing packages that require curl for installation... (github cli (gh); brave (brave-browser))"
 curl -fsS https://dl.brave.com/install.sh | sh
@@ -38,7 +40,7 @@ curl -fsS https://dl.brave.com/install.sh | sh
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
 	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
 	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&-& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
@@ -55,12 +57,18 @@ else
 	echo -e "\033[0;31m exited with code 1 \033[0m"
 	exit 1
 fi
-sleep 1
+sleep 2
 clear
 echo "installing and compiling blobby-volley2 v1.1.1..."
-tar -xvf 'blobby2-linux-1.1.1.tar.gz'
-mv 'blobby-1.1.1' '~/'
-cd '~/blobby-1.1.1/'
+tar -xvf blobby2-linux-1.1.1.tar.gz
+mv blobby-1.1.1 ~/
+cd ~/blobby-1.1.1/
 cmake .
 make install
 echo "finished installing blobby-volley v1.1.1!"
+sleep 2
+clear
+echo "cleaning everything up..."
+cat ~/start-position | rm -rf 
+rm -rf ~/start-position
+echo "self termination complete!!"
